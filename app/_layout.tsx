472db342ebea0a5afaa44478cbca66d6510ff7d5
Prashant <prashan1837d@gmail.com>
2025-11-07 00:04:50 +0530

    refactor: streamline font loading and splash screen handling in RootLayout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,27 @@
+import { useFonts } from 'expo-font';
 import { Stack } from "expo-router";
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from "react";
 import "./global.css";
 
+SplashScreen.preventAutoHideAsync();
+
 export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    'outfit': require('../assets/fonts/Outfit-Regular.ttf'),
+    'outfit-medium': require('../assets/fonts/Outfit-SemiBold.ttf'),
+    'outfit-bold': require('../assets/fonts/Outfit-Bold.ttf'),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return <Stack screenOptions={{ headerShown: false }} />;
 }
